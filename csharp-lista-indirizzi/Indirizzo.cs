c367ad079bd650de01be081be1badfe981fe0cf9
StefanoM98 <Utente@PC-STEFANO>
2023-09-14 15:18:52 +0200

    Classe Indirizzo aggiunta

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo
+    {
+        public string Name { get; set; }
+        public string Cognome { get; set; }
+        public string Strada { get; set; }
+        public string Città { get; set; }
+        public string Provincia { get; set; }
+        public int Cap { get; set; }
+
+        public Indirizzo (string nome, string cognome, string strada, string città, string provincia, int cap) 
+        {
+            Name = nome;
+            Cognome = cognome;
+            Strada = strada;
+            Città = città;
+            Provincia = provincia;
+            Cap = cap;
+        }
+
+        
+    }
+}
